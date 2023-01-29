288b42500f97c02e020a306e7a4481154c127b83
Alexander <klivets.alex@gmail.com>
2023-01-29 12:18:44 +0300

    feat: add style header

--- source/js/index.js
+++ source/js/index.js
@@ -1,1 +1,18 @@
-console.log('hello')
+const buttonMenu = document.querySelector('.header__button');
+const menuBackground = document.querySelector('.header__wrapper');
+const menuNavigation = document.querySelector('.main-nav');
+
+buttonMenu.addEventListener('click', () => {
+  menuNavigation.classList.toggle('main-nav--active');
+  buttonMenu.classList.toggle('header__button--close');
+  menuBackground.classList.toggle('header__wrapper--active')
+})
+
+menuBackground.addEventListener('click', () => {
+  if (buttonMenu.classList.contains('header__button--close')) {
+    buttonMenu.classList.remove('header__button--close');
+    menuNavigation.classList.remove('main-nav--active');
+    menuBackground.classList.remove('header__wrapper--active');
+  }
+})
+
